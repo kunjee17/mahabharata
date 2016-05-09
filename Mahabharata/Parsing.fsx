a89a393367799dce622a692ab4819da3f65651b3
Kunjan Dalal <kunjee17@gmail.com>
2016-05-09 23:37:24 +0530

    name and dialog actors seperated based on asked word

--- Mahabharata/Parsing.fsx
+++ Mahabharata/Parsing.fsx
@@ -1,8 +1,7 @@
-﻿#load """NameFinder.fs"""
-#I """../packages/fparsec/lib/net40-client"""
-#r """FParsec.dll"""
-#r """FParsecCS.dll"""
-
+﻿#load "NameFinder.fs"
+#I "../packages/fparsec/lib/net40-client"
+#r "FParsec.dll"
+#r "FParsecCS.dll"
 
 open FParsec
 
@@ -15,22 +14,39 @@ wealth is asceticism, we desire, O Brahmana, to hear in detail everything
 as it happened. Indeed, our curiosity hath become great.'"""
 
 
-let parseAllExceptQuote = charsTillString "'" false 1000
-let parseQuotedString = 
-    skipCharsTillString "'" true 1000 >>. parseAllExceptQuote .>> pstring "'"
+let clearStr = NameFinder.removeSpecialChars txt + "@"
 
 
-let parsedQuote = run parseQuotedString txt
+let parseAllExceptQuote = charsTillString "@" false 1000
+let parseQuotedString = skipCharsTillString "asked" true 1000 >>. parseAllExceptQuote
+let parsedQuote = run parseQuotedString clearStr
 
-let quote p =
-  match p with
-  | Success(q,_,_) -> q
-  | Failure(_,_,_) -> "Could not parse string"
+let quote p = 
+    match p with
+    | Success(q, _, _) -> q
+    | Failure(_, _, _) -> "Could not parse string"
 
 let words = (quote parsedQuote) |> NameFinder.names
 
-let matches =
-  [ for word in words do
-      if NameFinder.namesOnly |> Array.contains word
-      then yield word ]
-              
+let matches words= 
+    [| for word in words do
+          if NameFinder.namesOnly |> Array.contains word then yield word |]
+
+let actorDialog sen = 
+    let clearStr = NameFinder.removeSpecialChars sen + "@" //adding @ to find the end of string as all special char removed
+    
+    let actor = 
+        run (charsTillString "asked" false 1000) clearStr |> fun p -> 
+            match p with
+            | Success(q, _, _) -> q
+            | Failure(_) -> failwith "can't parse string"
+            |> NameFinder.names |> matches |> Array.tryHead |> fun x -> x.Value
+    let dialog = 
+        run (skipCharsTillString "asked" true 1000 >>. charsTillString "@" false 1000) clearStr |> fun p -> 
+            match p with
+            | Success(q, _, _) -> q
+            | Failure(_) -> failwith "can't parse string"
+            |> NameFinder.names |> matches |> Array.countBy id
+    (actor, dialog)
+
+actorDialog txt
