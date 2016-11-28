6370dba0cd15497acae5cf524f1145bd17e01682
Kunjan Dalal <kunjee17@gmail.com>
2016-11-28 17:40:53 +0530

    Done finding unique terms with frequency for all the books

--- Mahabharata/Lesson1.fsx
+++ Mahabharata/Lesson1.fsx
@@ -1,10 +1,25 @@
 (**
 Lesson 1 - Basic understanding of NLP. Getting started with term,document frequency for term and Inverse document frequency for term. 
 *)
-
+#I "../packages"
+#r "Microsoft.FsharpLu.Json/lib/net452/Microsoft.FsharpLu.Json.dll"
+#r "Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
 open System.IO
 open System
 open System.Text.RegularExpressions
+open Newtonsoft.Json
+open Microsoft.FSharpLu.Json
+
+
+type DocItem = {
+    Term : string 
+    Freq : int
+}
+
+type Doc = {
+    Bookno : string
+    DocItems : DocItem []
+}
 
 
 type Book(bookno:string) = 
@@ -36,17 +51,25 @@ let booknos = [|"01";"02";"03";"04";"05";"06";"07";"08";"09";"10";"11";"12";"13"
 
 let books = [|
     for n in booknos do
-        yield new Book(n) 
+        yield Book(n) 
 |]
 
 
-let k = 1
-let allUniqueTermsbut1 = books.[k..] |> Array.map (fun x -> x.BookUniqueTerms) |> Array.fold (fun acc elem -> acc + elem) Set.empty
+let getUniqueTermsByFrequencyForBook (bookno : string) : Doc = 
+    let bookToProcess = books |> Array.find (fun x -> x.BookNo = bookno)
+    let allOtherBooks = books |> Array.filter (fun x -> x.BookNo <> bookno)
 
-let termsspecificto1 = books.[0] |> fun x -> x.BookUniqueTerms - allUniqueTermsbut1
+    let allBookTerms = allOtherBooks |> Array.map (fun x -> x.BookUniqueTerms) |> Array.fold (fun acc elem -> acc + elem) Set.empty
 
+    let termsSpecifictoBook = bookToProcess.BookUniqueTerms - allBookTerms
+    let docItems: DocItem [] = bookToProcess.BookTermGroup 
+                                |> Array.filter (fun (x,_) -> termsSpecifictoBook.Contains x) 
+                                |> Array.map (fun (x,y)-> { Term = x; Freq = y})
+    {Bookno = bookno; DocItems = docItems}
+    
 
+let uniqeTermsforAllBooks = booknos |> Array.map getUniqueTermsByFrequencyForBook 
 
-termsspecificto1 |> Set.toArray |> Array.take 50 |> Array.iter (printfn "%A") 
+Compact.serialize uniqeTermsforAllBooks
 
 
