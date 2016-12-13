0b1e9fddb06335a811ab840bebd545e3e655b267
Kunjan <kunjee17@gmail.com>
2016-12-13 19:52:10 +0530

    Book list added to main page.

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,9 +1,9 @@
-var jumboHeight = $('.jumbotron').outerHeight();
-function parallax(){
-    var scrolled = $(window).scrollTop();
-    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
-}
+// var jumboHeight = $('.jumbotron').outerHeight();
+// function parallax(){
+//     var scrolled = $(window).scrollTop();
+//     $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+// }
 
-$(window).scroll(function(e){
-    parallax();
-});
+// $(window).scroll(function(e){
+//     parallax();
+// });
